903f49129711ec974287a5a22763432d5a61a558
hhshhhskk <hhshhhskk@naver.com>
2023-06-24 23:22:40 +0900

    test

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -20,20 +20,20 @@ router.post('/signup/insert', (req, res) => {
   const { name, id, password, passwordcheck } = req.body;
   // id, password, name이 있는지 체크한다.
   if ( !name || !id || !password || !passwordcheck ) {
-    res.status(400).json({ message: 'id, password, passwordcheck, name은 필수입력 사항입니다.' });
+    res.status(400).json({ status: '실패', message: 'id, password, passwordcheck, name은 필수입력 사항입니다.' });
     return;
   }
 
   // password와 passwordcheck는 같아야한다.
   if (password !== passwordcheck) {
-    res.status(400).json({ message: '패스워드가 일치하지않습니다.' });
+    res.status(400).json({ status: '실패', message: '패스워드가 일치하지않습니다.' });
     return;
   }
 
   // id는 중복되지 않도록한다.
   db.query(`SELECT id FROM member WHERE id = "${id}"`, function (err, rows) {
     if (rows.length !== 0) {      
-      res.status(400).json({ message: '이미 존재하는 아이디입니다.' });
+      res.status(400).json({ status: '실패', message: '이미 존재하는 아이디입니다.' });
     return;
     } else {
   // 중복이 없다면 회원을 추가한다.
@@ -41,7 +41,7 @@ router.post('/signup/insert', (req, res) => {
         if (err) {
           console.log(err);
         } else {
-          res.json({ message: '회원가입이 완료되었습니다.' })
+          res.json({ status: '성공', message: '회원가입이 완료되었습니다.' })
           return;
         }
       });
