f4d3a427590fb96de62712447cbb3c9c41002e3a
hhshhhskk <hhshhhskk@naver.com>
2023-06-29 22:05:13 +0900

    로그인 로그아웃구현

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 const db = require('../config/database');
 
 // 
-router.get('/', function(req, res){
+router.get('/', (req, res) => {
   // res.send('Error');
   db.query('select * from member', function (err, rows) {
       if(err){
@@ -47,10 +47,22 @@ router.post('/signup/insert', (req, res) => {
       });
     }
   });
+});
 
+// 로그인
+router.post('/login', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const { id, password } = req.body;
 
-  
-
+  db.query(`select name from member WHERE id = "${id}" AND password = "${password}"`, function (err, rows) {
+    if (rows.length === 0) {
+      res.json({ status: '실패', message: '아이디 혹은 비밀번호가 일치하지 않습니다.' })
+      return;
+    } else {
+      res.json(rows);
+      return;
+      }
+    });
 });
 
 module.exports = router;
